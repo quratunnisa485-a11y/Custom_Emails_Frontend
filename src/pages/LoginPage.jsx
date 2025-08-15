15a3653b6d38a5db5a05ae8af24e834834917c42
Qurat Un Nisa <quratunnisa485@gmail.com>
2025-08-15 11:41:45 +0500

    finallllllllll

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -50,7 +50,7 @@ const Login = () => {
           <h1>Welcome!</h1>
           <p>
             Effortlessly write, manage, and send custom emails all from one
-            place.Choose your sender mail, type your message, and hit send.
+            place.<p>Choose your <br>sender mail, type your message, and hit send.</br></p>
           </p>
         </div>
 
